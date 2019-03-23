a9cb68ee7e95e9a1ac63e3957bca4b8c6218ca01
James Cho <hwisoocho@gmail.com>
2019-03-23 12:13:00 -0700

    add shared data

---
+++ shared/players.js
@@ -0,0 +1,7 @@
+export const PLAYERS = [
+  { id: 0, name: "Michael Jordan", image: "images/jordan.jpg" },
+  { id: 1, name: "Magic Johnson", image: "images/magic.jpg" },
+  { id: 2, name: "Larry Bird", image: "images/bird.jpg" },
+  { id: 3, name: "Kareem Abdul-Jabbar", image: "images/kareem" },
+  { id: 4, name: "Julius Erving", image: "images/julius" }
+];
